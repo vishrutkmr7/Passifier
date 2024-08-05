28e972042a273d2d7df20f1f4e0f0c50bdbc9570
Vishrut Jha <me@vishrutjha.com>
2024-08-04 23:45:37 -0700

    Initial Commit

---
+++ Passifier/PassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassifierApp.swift
+//  Passifier
+//
+//  Created by Vishrut Jha on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PassifierTests/PassifierTests.swift
@@ -0,0 +1,17 @@
+//
+//  PassifierTests.swift
+//  PassifierTests
+//
+//  Created by Vishrut Jha on 8/4/24.
+//
+
+import Testing
+@testable import Passifier
+
+struct PassifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
